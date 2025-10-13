b0a2489c595226280667cd196eb4c5020c0c0097
tantong0402 <tantong0402@gmail.com>
2025-10-13 14:52:48 +0700

    Tách nhánh web

--- main/lib/server/include/payload_format.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PAYLOAD_FORMAT__
-#define __PAYLOAD_FORMAT__
-#include <Arduino.h>
-#include <stdint.h>
-
-String PayloadFormat(float temperature, float humidity, float pm25, float gas);
-
-#endif
